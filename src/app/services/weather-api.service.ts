e18c1a119e3036047553162e93702abd40991bc2
ralucamicu <ralucamicu158@gmail.com>
2022-04-21 14:09:57 +0300

    nothing changed

--- src/app/services/weather-api.service.ts
+++ src/app/services/weather-api.service.ts
@@ -1,4 +1,4 @@
-//makes api requests
+//makes http requests
 //allows to move all of those HTTP requests the app makes into one file that can then be called inside any .component.ts file 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';  //make HTTP requests  
@@ -9,28 +9,13 @@ import { HttpClient } from '@angular/common/http';  //make HTTP requests
 })
 
 export class WeatherApiService {
-
-  //----implementate din angular-----
-  // geoCoordsURL = 'http://api.openweathermap.org/geo/1.0/direct?limit=1&appid=f77221ff9711060bd0cc9778bc441b3b&exclude=id&q=';
-  
-  // getGeoLoc(location: any): Promise<any>{
-  //   return this.http.get(this.geoCoordsURL + location).toPromise();
-  // }
-
-  // getForecast(latitude: number, longitude: number): Promise<any>{
-  //   return this.http.get('https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=current,hourly,minutely'
-  //     + '&units=metric&appid=f77221ff9711060bd0cc9778bc441b3b'
-  //   ).toPromise();
-  // }
-
-
   //-----implementate din php-----
   getCity(city): Promise<any> {
-    return this.http.get<any>('http://127.0.0.1:8000/checkCity/' + city).toPromise();
+    return this.http.get<any>('http://127.0.0.1:8000/city/' + city).toPromise();
   }
 
   getForecast(city, lat, lon): Promise<any>{
-    return this.http.get('http://127.0.0.1:8000/checkForecast/' + city + '/' + lat + '/' + lon).toPromise();
+    return this.http.get('http://127.0.0.1:8000/forecast/' + city + '/' + lat + '/' + lon).toPromise();
   }
   constructor(private http: HttpClient) { 
   }
